a2e128624877f98176a790259c5897b98d4a72a6
QusayAbusundus <32073337+QusayAbusundus@users.noreply.github.com>
2018-01-22 13:39:38 -0500

    added bobbing pickups

--- Unity Ball Game/Assets/Scripts/Bobbing.cs
+++ Unity Ball Game/Assets/Scripts/Bobbing.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Bobbing : MonoBehaviour {
 	
-	
+	private int direction = 1;
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		transform.Rotate(new Vector3(15, 30, 25) * Time.deltaTime);
-		//if
-		//transform.Translate(new Vector3()
+
+		transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y + Random.Range(0.001f, 0.1f)*direction, transform.localPosition.z);
+		if(transform.localPosition.y > 2)
+		{
+			direction = -1;
+		}
+		else if(transform.localPosition.y <= 0.5f)
+		{
+			direction = 1;
+		}
 	}
 }
